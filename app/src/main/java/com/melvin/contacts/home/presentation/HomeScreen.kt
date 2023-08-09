dbf944c10592136450aa5d19073cd7883fd9b1ff
Melvin <melvinalek@gmail.com>
2023-08-09 09:55:28 -0300

    Hilt and Navigation set up

---
+++ app/src/main/java/com/melvin/contacts/home/presentation/HomeScreen.kt
@@ -0,0 +1,17 @@
+package com.melvin.contacts.home.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+        }
+    }
+}
