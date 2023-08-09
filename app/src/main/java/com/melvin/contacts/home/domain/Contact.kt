ee642d2f031df8739c1975d9e9d22500035164af
Melvin <melvinalek@gmail.com>
2023-08-09 09:06:12 -0300

    Create contact row

---
+++ app/src/main/java/com/melvin/contacts/home/domain/Contact.kt
@@ -0,0 +1,18 @@
+package com.melvin.contacts.home.domain
+
+data class Contact(
+    val firstName: String,
+    val lastName: String,
+    val companyName: String,
+    val address: String,
+    val city: String,
+    val country: String,
+    val state: String,
+    val zip: String,
+    val phone: String,
+    val phone1: String,
+    val email: String,
+) {
+    val fullName get() = "$firstName $lastName"
+    val initials get() = getInitials(fullName)
+}
---
+++ app/src/main/java/com/melvin/contacts/home/presentation/components/ContanctRow.kt
@@ -0,0 +1,80 @@
+package com.melvin.contacts.home.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.melvin.contacts.home.domain.Contact
+
+@Composable
+fun ContactRow(
+    contact: Contact,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit,
+) {
+    Card(
+        modifier = modifier
+            .height(46.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .clickable {
+                onClick("")
+            },
+        shape = CardDefaults.elevatedShape,
+        colors = CardDefaults.cardColors(
+            containerColor = LightGray
+        ),
+        elevation = CardDefaults.elevatedCardElevation()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 10.dp, end = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            VisitorInitials(contact.initials)
+
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = contact.fullName,
+                color = DarkGray,
+                fontWeight = FontWeight.Medium,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContactRowPreview() {
+    ContactRow(
+        Contact(
+            "Melvin",
+            "Kucuk",
+            "Right Balance",
+            "Fake Street 123",
+            "Fake Street 123",
+            "Argentina",
+            "Buenos Aires",
+            "1333",
+            "11432343",
+            "112345678",
+            "mkucuk@gmail.com",
+        )
+    ) {}
+}
